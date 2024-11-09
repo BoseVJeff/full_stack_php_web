1c344fd89b4a33d7e76f02218e4680758a4b6202
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2024-11-09 22:16:46 +0530

    Added doc strings and updated formatting

--- src/api/base.php
+++ src/api/base.php
@@ -1,48 +1,55 @@
-<?php declare(strict_types=1);
+<?php declare (strict_types = 1);
 
 // The directory to upload the users' files.
-// 
+//
 // Note that each user will additionally have a folder of their name generated.
-$uploads_dir="../uploads/";
+$uploads_dir = "../uploads/";
 
 /**
  * An enum-ish class with types of permissions and their values.
  * A higher value idicates a greater amount of access.
- * 
+ *
+ * The permission levels are numerical to allow ease of comparision.
+ *
  * A permission with a higher value implies all permissions with lower values too.
  * i.e. A user with permission 100 can also perform 50-level actions.
- * 
+ *
  * Note that the gaps exist to allow for more permissions in the future.
  */
 class Permission
 {
     public int $level;
 
-    function __construct(int $permission_level)
+    /**
+     * Initialises the class with the specified permission level.
+     *
+     * @param int $permission_level The numerical value of the permission level
+     */
+    public function __construct(int $permission_level)
     {
-        $this->level=$permission_level;
+        $this->level = $permission_level;
     }
 
     /**
      * No access allowed.
      */
-    static function no_access():Permission
+    public static function no_access(): Permission
     {
         return new Permission(0);
     }
-    
+
     /**
      * Only read access allowed.
      */
-    static function read_only():Permission
+    public static function read_only(): Permission
     {
         return new Permission(10);
     }
-    
+
     /**
      * Read and write access allowed.
      */
-    static function read_write():Permission
+    public static function read_write(): Permission
     {
         return new Permission(100);
     }
@@ -50,27 +57,36 @@ class Permission
     /**
      * Returns true only if an action requiring `action_permission` can be performed
      * by a user having `$this` permission.
-     * 
-     * In practice, this is valudated by checking that 
+     *
+     * In practice, this is valudated by checking that
      */
-    function actionAllowed(Permission $action_permission):bool
+    public function actionAllowed(Permission $action_permission): bool
     {
         return ($this->level) >= ($action_permission->level);
     }
 }
 
-class Token {
+class Token
+{
     public string $token;
     public ?string $label;
 
-    function __construct(string $token, ?string $label) {
-        $this->token=$token;
-        $this->label=$label;
+    /**
+     * @param string $token
+     * @param string $label
+     */
+    public function __construct(string $token, ?string $label)
+    {
+        $this->token = $token;
+        $this->label = $label;
     }
 
-    public static function generate(?string $label=null):Token {
-        $tok=uniqid("tfs-",false);
-        return new Token($tok,$label);
+    /**
+     * @param string $label
+     */
+    public static function generate(?string $label = null): Token
+    {
+        $tok = uniqid("tfs-", false);
+        return new Token($tok, $label);
     }
 }
-?>
